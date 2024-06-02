384c3f2be47fac30016c9c7598905af334f6982f
Ben Bourque <bbourq7@gmail.com>
2024-06-02 09:39:03 -0400

    updated SETTINGS file

--- bash_game_project/bash_game_project/settings.py
+++ bash_game_project/bash_game_project/settings.py
@@ -26,12 +26,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zx%652436^=m+jgqwq&nx^2f&qmpkv)le-53_pft1ryfz8qygm'
+SECRET_KEY = os.environ.get('SECRET_KEY') #'django-insecure-zx%652436^=m+jgqwq&nx^2f&qmpkv)le-53_pft1ryfz8qygm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True #os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gamepanda-68fb779724ae.herokuapp.com',
+                 '127.0.0.1',
+                 ]
 
 
 # Application definition
@@ -156,8 +158,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'postharvest0420@gmail.com'
-EMAIL_HOST_PASSWORD = 'dtss dcaq nkys owxi'
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 
 
 
